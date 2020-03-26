6c6ed4f3552d95ebc87317c9dd4264a5f2d4b517
unknown <heyvitoria.lopes@gmail.com>
2020-03-26 11:58:32 -0300

    página de casos estruturada

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    // <View>
+    //   <Text>xau</Text>
+    // </View>
+    <Routes />
+  );
+}
